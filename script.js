50713733ab8fb41bff46fec563c08ac9f3ec4f62
Cole Wilson <cole@colewilson.xyz>
2022-08-10 12:01:17 -0700

    big update

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 var h, m, lastm;
 var colors = [
+	// "0, 0, 0",
 	"255, 0, 0",
 	"0, 255, 0",
 	"0, 0, 255",
 	"255, 255, 255",
 	"255, 255, 0",
 	"0, 255, 255",
-	"255, 0, 255"
-], colorindex = localStorage.getItem("color")-1||0;
+	"255, 0, 255",
+];
 
-function getfactors(n) {
-	if (n == 0) return [0];
-	var out = [];
-	for (var i=0;i<=n;i++)
-		if (n%i==0)
-			out.push(i);
-	return out;
-}
+var settings = JSON.parse(localStorage.getItem("settings")) || {
+	hour_color: 0,
+	minute_color: 0,
+	bg_color: 0,
+	addition: true,
+	subtraction: true,
+	multiplication: true,
+	custom: false,
+	equations: "",
+};
+
 function equation(n, type) {
-	var a, b, c, out;
-	switch (type||["+", "-", "*"][Math.floor(Math.random()*3)]) {
+	function getfactors(n) {
+		if (n == 0) return [0];
+		var out = [];
+		for (var i=0;i<=n;i++)
+			if (n%i==0)
+				out.push(i);
+		return out;
+	}
+	var a, b, c, out, types = [];
+
+	if (settings.addition) types.push("+");
+	if (settings.subtraction) types.push("-");
+	if (settings.multiplication) types.push("*");
+	if (settings.custom && settings.equations) types.push("custom")
+
+
+
+	switch (type||types[Math.floor(Math.random()*types.length)]) {
 		case "+":
 			a = Math.floor(Math.random()*n);
 			b = n - a;
@@ -38,7 +58,7 @@ function equation(n, type) {
 			}
 			break;
 		case "-":
-			a = Math.floor(Math.random()*(99-n)) + n; 
+			a = Math.floor(Math.random()*(99-n)) + n;
 			b = a - n;
 			if ((""+a+b).length == 2 && Math.random() > 0.4) {
 				c = (-a) + Math.floor(Math.random()*10);
@@ -60,29 +80,33 @@ function equation(n, type) {
 			b = n/a;
 			if (a == 0) b = 0;
 			out = a+"*"+b;
-			// if (out.length == 3 && Math.random() > 0.4 && type != "*") {
-			// 	c = Math.floor(Math.random()*19)-9;
-			// 	c = Math.max(1-n, c);
-			// 	let need = n + c;
-			// 	let count = 0;
-			// 	if (c<0) c = "+"+Math.abs(c);
-			// 	else c*= -1;
-			// 	while ((out = equation(need, "*")+c).length != 5 && count < 50) {count++}
-			// }
 			break;
+		case "custom":
+			let equations = settings.equations.split("\n");
+			for (var i=0;i<equations.length;i++) {
+				let parts = equations[i].split(" = ");
+				if (parts[0] == n) {
+					out = parts[1]
+					break
+				}
+			}
+			break;
+		default:
+			out = ""+n;
 	}
-	console.log(out, "=>", n);
-	if (eval(out) != n)
-		throw new Error(out + " did not equal " + n + "!!!");
 	return out;
 }
 
 function update() {
+	document.documentElement.style.setProperty('--bgcolor', colors[settings.bg_color])
+	document.documentElement.style.setProperty('--mincolor', colors[settings.minute_color])
+	document.documentElement.style.setProperty('--hourcolor', colors[settings.hour_color])
+
 	let now = new Date();
 	if (now.getMinutes() == lastm)
 		return;
 	lastm = now.getMinutes();
-	
+
 	var hour = now.getHours() % 12;
 	if (hour==0) hour = 12;
 	h.innerText = (equation(hour).padStart(5, "!"));
@@ -91,34 +115,70 @@ function update() {
 	m.innerText = (equation(minute)).padStart(5, "!");
 }
 window.onload = () => {
-	document.querySelector("button").onclick = function() {
-		this.remove();
-		let elem = document.querySelector("body");
-		if (elem.requestFullscreen) {
-	    elem.requestFullscreen();
-		} else if (elem.webkitRequestFullscreen) { /* Safari */
-			elem.webkitRequestFullscreen();
-		} else if (elem.msRequestFullscreen) { /* IE11 */
-			elem.msRequestFullscreen();
-		}
-	}
 	h = document.getElementById("hours");
 	m = document.getElementById("minutes");
-	setInterval(update, 900);
-	document.documentElement.style.setProperty('--color', colors[colorindex])
-}
-window.onclick = () => {
-	colorindex++;
-	if (colorindex >= colors.length)
-		colorindex = 0;
-	document.documentElement.style.setProperty('--color', colors[colorindex]);
-	localStorage.setItem("color", colorindex);
-}
-window.oncontextmenu = () => {
-	window.onclick();
-	return false;
-}
-window.onkeypress = () => {
-	lastm = 0;
 	update();
+
+	document.querySelector("#closewelcome").onclick = function() {
+		document.querySelector("#welcome").remove();
+	}
+
+	let settingEls = document.querySelectorAll(".setting");
+	for (var i=0;i<settingEls.length;i++) {
+		let el = settingEls[i];
+		console.log(el)
+		if (el.checked !== undefined)
+			el.checked = settings[el.name]
+		else
+			el.value = settings[el.name]
+		if (el.name == "equations")
+			el.hidden = !settings.custom
+		el.oninput = (e) => {
+			if (e.target.checked !== undefined) {
+				settings[e.target.name] = e.target.checked;
+				if (e.target.name == "custom") document.querySelector("[name=equations]").hidden = !e.target.checked
+			}
+			else
+				settings[e.target.name] = e.target.value
+			localStorage.setItem("settings", JSON.stringify(settings))
+		}
+	}
+
+	setInterval(update, 900);
+}
+
+// document.querySelector("body").onclick = (e) => {
+// 	e.stopPropagation();
+// 	settings.bg_color = (settings.bg_color+1)%colors.length;
+// 	localStorage.setItem("settings", JSON.stringify(settings))
+// 	update();
+// }
+document.querySelector("#hours").onclick = ()=> {
+	settings.hour_color = (settings.hour_color+1)%colors.length;
+	localStorage.setItem("settings", JSON.stringify(settings))
+	update();
+}
+document.querySelector("#minutes").onclick = () => {
+	settings.minute_color=(settings.minute_color+1)%colors.length;
+	localStorage.setItem("settings", JSON.stringify(settings))
+	update();
+}
+
+
+window.onkeypress = (e) => {
+	if (e.target.closest("#welcome") != null)
+		return;
+	lastm=0;
+	update();
+}
+window.ondblclick = (e) => {
+	if (e.target.closest("#welcome") != null)
+		return
+	let el = document.documentElement;
+	el.requestFullscreen()||el.webkitRequestFullScreen()||el.msRequestFullscreen()
+}
+window.onclick = (e) => {
+	if (e.target.closest("#welcome") != null)
+		return;
+	document.querySelector("#welcome").style.display = "none";
 }
